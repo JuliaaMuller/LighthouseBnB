4ebec64d7d6279fd60f89d82c866849ca34a36bc
JuliaaMuller <julialm.muller@gmail.com>
2022-02-10 19:46:15 -0500

    All queries functions reworked using database

--- LightBnB/LightBnB_WebApp-master/server/database.js
+++ LightBnB/LightBnB_WebApp-master/server/database.js
@@ -17,11 +17,11 @@ const db = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  const queryString = `
+  let queryString = `
   SELECT *
   FROM users
   WHERE email = $1;`
-  const values = [email];
+  let values = [email];
 
   return db.query(queryString, values)
   .then((res) => res.rows[0])
@@ -35,11 +35,11 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  const queryString = `
+  let queryString = `
   SELECT *
   FROM users
   WHERE id = $1;`;
-  const values = [id];
+  let values = [id];
 
   return db.query(queryString, values)
   .then((res) => res.rows[0])
@@ -53,11 +53,11 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const queryString = `
+  let queryString = `
   INSERT INTO users(name, email, password)
   VALUES ($1, $2, $3)
   RETURNING *;`;
-  const values = [user.name, user.email, user.password];
+  let values = [user.name, user.email, user.password];
 
   return db.query(queryString, values)
   .then(res => res.rows[0])
@@ -74,7 +74,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  let queryString = `
+  SELECT * FROM reservations
+  WHERE guest_id = $1
+  LIMIT $2;
+  `
+  let values = [guest_id, limit];
+
+  return db.query(queryString, values)
+  .then(res => res.rows)
+  // .catch(err => console.log(err));
 }
 exports.getAllReservations = getAllReservations;
 
@@ -87,22 +96,64 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 
- const getAllProperties = (options, limit = 10) => {
+ const getAllProperties = function (options, limit = 10) {
   
-  const queryString = `
-  SELECT properties.*, avg(property_reviews.rating) as average_rating 
-  FROM properties, property_reviews
+  // return db.query(`SELECT * FROM properties`)
+  // .then(res => res.rows)
+  // 1
+  let queryParams = [];
+  let filters = []
+  // 2
+  let queryString = `SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id`;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`${options.city}`);
+    filters.push(` city LIKE $${queryParams.length} `);
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    filters.push(` user LIKE $${queryParams.length} `);
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    filters.push(` cost_per_night > ($${queryParams.length})*100`);
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`);
+    filters.push(` cost_per_night < ($${queryParams.length})*100`);
+  }
+
+  if (filters.length) {
+    queryString += `WHERE ${filters.join(' AND ')}`;
+  }
+
+  // 4
+  
+  queryString += `
   GROUP BY properties.id
+  `;
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += ` HAVING avg(property_reviews.rating) > $${queryParams.length} `;
+  }
+  queryParams.push(limit);
+
+  queryString += `
   ORDER BY cost_per_night
-  LIMIT $1;
-  `
-  const values = [limit]
-  
-  return db.query(queryString, values)
-    .then(res => res.rows)
-    .catch((err) => {
-      console.log(err.message);
-    });
+  LIMIT $${queryParams.length};`
+
+  // 5
+  // console.log(queryString, queryParams);
+
+  // 6
+  return db.query(queryString, queryParams)
+  .then(res => res.rows);
 };
 exports.getAllProperties = getAllProperties;
 
@@ -112,7 +163,7 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
+ const addProperty = function(property) {
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
